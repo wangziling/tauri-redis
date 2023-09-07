468d96b3fa19837719dcfcb18f8babeed5b078df
王子凌 <sling1994@live.com>
2023-09-07 13:34:59 +0800

    fix(tauri): fix the wrong proc_attribute_macro usage

--- src-tauri/app/command/demo.rs
+++ src-tauri/app/command/demo.rs
@@ -1,8 +1,3 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-
 use crate::features::error;
 use serde::Serialize;
 use std::time::Instant;
