e26bbda5824dedc7f6cdc3e86fae0f56493e37b3
王子凌 <sling1994@live.com>
2023-09-21 12:02:53 +0800

    chore(front): disabled ssr for the main page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,6 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+// Add this, or 'window' will be an undeclared variable.
+export const ssr = false;
