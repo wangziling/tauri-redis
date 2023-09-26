71dffb6db0cc21fa2d503515bc48830291464e35
王子凌 <sling1994@live.com>
2023-09-26 10:13:45 +0800

    chore(front): integrate with the 'list_client_metrics' command

--- src/lib/apis/redis-client.ts
+++ src/lib/apis/redis-client.ts
@@ -1,20 +1,6 @@
 import type { IpcClientMetrics, IpcConnection, IpcResponse } from '$lib/types';
 import { fetchIpc } from '$lib/utils/async';
 
-export function fetchListRedisClientMetrics(guid: IpcConnection['guid']): Promise<IpcResponse<IpcClientMetrics>> {
-	return fetchIpc<string>('list_client_metrics', { guid }).then(function (res) {
-		const resData = res.data;
-
-		let trueData = {};
-		if (typeof resData === 'string') {
-			try {
-				trueData = JSON.parse(resData);
-			} catch (e) {}
-		}
-
-		return {
-			...res,
-			data: trueData
-		};
-	});
+export function fetchListRedisClientMetrics(guid: IpcConnection['guid']) {
+	return fetchIpc<IpcClientMetrics>('list_client_metrics', { guid });
 }
