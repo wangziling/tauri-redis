3b9c4b529ec4e54db81c1f42df44696619103497
王子凌 <sling1994@live.com>
2023-09-27 19:14:02 +0800

    feat(front): enable a base component named 'Card'

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { calcDynamicClasses } from '$lib/utils/calculators';
+
+	export let shadowStill = true;
+
+	$: dynamicClasses = calcDynamicClasses([
+		'card',
+		{
+			'card--shadow-still': shadowStill
+		},
+		$$restProps.class
+	]);
+</script>
+
+<div class={dynamicClasses}>
+	<div class="card-wrapper">
+		{#if $$slots.header}
+			<div class="card__header">
+				<slot name="header" />
+			</div>
+		{/if}
+		<div class="card__body">
+			<slot />
+		</div>
+	</div>
+</div>
