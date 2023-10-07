f4b16e46c13e8a0e5da83bf401063e92877c9151
王子凌 <sling1994@live.com>
2023-10-07 17:34:26 +0800

    chore(front): enable an option named 'stopPropagation' for the common 'error handler'

--- src/lib/utils/page.ts
+++ src/lib/utils/page.ts
@@ -1,12 +1,15 @@
 import { messageManager } from '$lib/components/notification/message';
 import { MessageType } from '$lib/types';
+import { get as lodashGet } from 'lodash-es';
 
-export function invokeErrorHandle(err: Error) {
+export function invokeErrorHandle(err: Error, options?: Partial<{ stopPropagation: boolean }>) {
 	messageManager.append({
 		content: err.message,
 		type: MessageType.Error,
 		closable: true
 	});
 
-	throw err;
+	if (!lodashGet(options, 'stopPropagation')) {
+		throw err;
+	}
 }
