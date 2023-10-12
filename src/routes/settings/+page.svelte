b1810df9655698e8907bdb9fadd22d68e6cee7a6
王子凌 <sling1994@live.com>
2023-10-12 18:59:21 +0800

    feat(front): enable a brief 'settings' route

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,4 +1,85 @@
 <script lang="ts">
+	import { translator } from 'tauri-redis-plugin-translation-api';
+	import { settings, Theme } from 'tauri-redis-plugin-setting-api';
+	import Card from '$lib/components/Card.svelte';
+	import Form from '$lib/components/form/Form.svelte';
+	import FormItem from '$lib/components/form/FormItem.svelte';
+	import Select from '$lib/components/Select.svelte';
+	import { writable } from 'svelte/store';
+	import { createThemeMisc } from '$lib/utils/appearance';
+
+	const themeMisc = createThemeMisc();
+
+	const translations = translator.derived(function () {
+		return {
+			settings: translator.translate('settings|Settings'),
+			'settings appearance': translator.translate('settings appearance|Appearance'),
+			'settings choose theme': translator.translate('settings choose theme|Choose theme')
+		};
+	});
+
+	const resources = settings.derived();
+
+	const model = writable({
+		theme: ''
+	});
+
+	const rules = writable({});
+
+	settings.subscribe((res) => {
+		model.update((m) => {
+			m.theme = res.theme;
+
+			return m;
+		});
+	});
+
+	settings.resources();
+
+	function handleThemeChange(e: CustomEvent<Theme>) {
+		switch (e.detail) {
+			case Theme.Dark: {
+				themeMisc.useDarkTheme();
+				break;
+			}
+			case Theme.Light: {
+				themeMisc.useLightTheme();
+				break;
+			}
+			case Theme.System: {
+				themeMisc.useSystemTheme();
+			}
+		}
+
+		return settings.set('theme', e.detail);
+	}
+
+	$: themesOptions = ($resources.themes || []).map((th) => ({
+		label: translator.translate(th.labelTranslationKey),
+		value: th.value
+	}));
 </script>
 
-<div class="tauri-redis-settings">Settings</div>
+<section class="tauri-redis-settings">
+	<div class="tauri-redis-settings-wrapper">
+		<div class="tauri-redis-settings-header settings-header">
+			<div class="settings-header-wrapper">
+				<h1 class="settings-header__caption">{$translations['settings']}</h1>
+			</div>
+		</div>
+		<div class="tauri-redis-settings-content">
+			<Card class="settings-card settings-card__appearance">
+				<div class="settings-card-header" slot="header">
+					<h4 class="settings-card-header__caption">{$translations['settings appearance']}</h4>
+				</div>
+				<div class="settings-card-content">
+					<Form class="settings-form settings-form__appearance" {model} {rules}>
+						<FormItem bind:label={$translations['settings choose theme']} prop="theme">
+							<Select options={themesOptions} on:input={handleThemeChange} />
+						</FormItem>
+					</Form>
+				</div>
+			</Card>
+		</div>
+	</div>
+</section>
