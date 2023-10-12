c962ad83f86cc9c9cc585b296dc27f9155627a0a
王子凌 <sling1994@live.com>
2023-10-12 18:58:52 +0800

    chore(front): integrate with the settings to restore the theme automatically

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { createThemeMisc } from '$lib/utils/appearance';
 	import { PageTheme } from '$lib/types';
 	import Aside from './Aside.svelte';
+	import { settings, Theme } from 'tauri-redis-plugin-setting-api';
 
 	const themeMisc = createThemeMisc();
 	themeMisc.initEvents();
@@ -12,6 +13,22 @@
 		});
 
 		document.documentElement.classList.add(`theme-${theme.toLowerCase()}`);
+	});
+
+	settings.getTheme().then((theme) => {
+		switch (theme) {
+			case Theme.Dark: {
+				themeMisc.useDarkTheme();
+				break;
+			}
+			case Theme.Light: {
+				themeMisc.useLightTheme();
+				break;
+			}
+			case Theme.System: {
+				themeMisc.useSystemTheme();
+			}
+		}
 	});
 </script>
 
