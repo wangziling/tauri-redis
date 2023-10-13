c98bd63d2274a254eb7b00d08912c5a82b4ee7b0
王子凌 <sling1994@live.com>
2023-10-13 12:04:29 +0800

    build(tauri): enhance the building events for the setting plugin

--- src-tauri/plugins/setting/webview-dist/webview-src/ipc.d.ts
+++
@@ -1,5 +0,0 @@
-import { Settings } from './types';
-export declare function resources(): Promise<Settings>;
-export declare function get(key: keyof Settings): Promise<any>;
-export declare function set(key: keyof Settings, value: any): Promise<void>;
-export declare function reset(): Promise<void>;
